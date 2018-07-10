d0122ee3fcc3bff4d37a95a8f7b9dfd03cf62234
Tomasz-Mankowski <tomasz.mankowski@put.poznan.pl>
2018-07-10 18:02:29 +0200

    Implemented features: AAC, DASDV, MAV1, MAV2, MAV , MAVSLP, VAR

--- features.py
+++ features.py
@@ -42,12 +42,65 @@ def features_from_xml(xml_file_url, hdf5_file_url):
     return feature_frame
 
 
-def feature_ZeroCross(series, window, step, deadzone):
+def feature_AAC(series, window, step):
+    """Average Amplitude Change"""
     windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
-    zc = np.apply_along_axis(lambda x: np.sum(np.diff(x[(x < -deadzone) | (x > deadzone)] > 0)), axis=1, arr=windows_strided)
-    return pd.Series(data=zc, index=series.index[indexes])
+    # TODO: Check if really should by divided by N, not N-1 (standard mean), verification needed
+    return pd.Series(data=np.divide(np.sum(np.abs(np.diff(windows_strided)), axis=1), window), index=series.index[indexes])
+
+
+def feature_DASDV(series, window, step):
+    """Difference Absolute Standard Deviation Value"""
+    windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
+    return pd.Series(data=np.sqrt(np.mean(np.square(np.diff(windows_strided)), axis=1)), index=series.index[indexes])
+
+
+def feature_MAV1(series, window, step):
+    """Modified Mean Absolute Value Type 1"""
+    windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
+    win_weight = [1 if ((0.25*window <= i) & (i <= 0.75*window)) else 0.5 for i in range(1, window+1)]
+    return pd.Series(data=np.mean(np.abs(windows_strided) * win_weight, axis=1), index=series.index[indexes])
+
+
+def feature_MAV2(series, window, step):
+    """Modified Mean Absolute Value Type 2"""
+    windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
+    # TODO: Phinyomark states that window weight for i > 0.75N is 4(i-N)/4, should be 4(N-i)/N, verification needed
+    win_weight = [1 if ((0.25*window <= i) & (i <= 0.75*window)) else (4*i/window) if (i < 0.25*window) else (4*(window-i)/window) for i in range(1, window+1)]
+    return pd.Series(data=np.mean(np.abs(windows_strided) * win_weight, axis=1), index=series.index[indexes])
+
+
+def feature_MAV(series, window, step):
+    """Mean Absolute Value"""
+    windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
+    return pd.Series(data=np.mean(np.abs(windows_strided), axis=1), index=series.index[indexes])
+
+
+def feature_MAVSLP(series, window, step):
+    """Mean Absolute Value Slope"""
+    # TODO: Mean Absolute Value Slope is considered as feature as set of k-segment diffs of adjectent MAV, here not the set/list but each diff is calculated, verification needed
+    windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
+    # It can be also defined as below, however it not practical:
+    # slp = np.diff(np.mean(np.abs(windows_strided), axis=1))
+    # stride = slp.strides[0]
+    # return pd.Series(data=as_strided(slp, shape=(len(slp) - segments + 1, segments), strides=(stride, stride)).tolist(), index=series.index[indexes[segments:]])
+    return pd.Series(data=np.diff(np.mean(np.abs(windows_strided), axis=1)), index=series.index[indexes[1:]])
 
 
 def feature_RMS(series, window, step):
+    """Root Mean Square"""
     windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
     return pd.Series(data=np.sqrt(np.mean(np.square(windows_strided), axis=1)), index=series.index[indexes])
+
+
+def feature_VAR(series, window, step):
+    """Variance"""
+    windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
+    return pd.Series(data=np.var(windows_strided, axis=1), index=series.index[indexes])
+
+
+def feature_ZC(series, window, step, deadzone):
+    """Zero Crossing"""
+    windows_strided, indexes = ut.moving_window_stride(series.values, window, step)
+    zc = np.apply_along_axis(lambda x: np.sum(np.diff(x[(x < -deadzone) | (x > deadzone)] > 0)), axis=1, arr=windows_strided)
+    return pd.Series(data=zc, index=series.index[indexes])
